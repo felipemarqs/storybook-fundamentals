d4be9ce5f262963ac0738e13a717e0b164eb41f9
Felipe Marques <felipe.marqs@outlook.com>
2024-05-11 14:37:23 +0000

    feat: :sparkles: Added Shad Button

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export const App = () => {
   return (
     <div>
       <h1>Hello Story Book!</h1>
+
+      <Button>Confirmar</Button>
     </div>
   );
 };
